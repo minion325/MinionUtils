a3351c145035305f2c23f71726ad16cabc7998b8
minion325 <saifmohammed966@gmail.com>
2018-09-15 09:25:42 -0400

    first commit

---
+++ src/org/guildcraft/minion325/utils/MinionUtils.java
@@ -0,0 +1,11 @@
+package org.guildcraft.minion325.utils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinionUtils extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
